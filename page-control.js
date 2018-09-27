a6f05b755af9e9681820f7156ae56b8026596f96
Steve Johnson <steve@brightec.co.uk>
2018-09-27 17:04:27 +0100

    highlight navigation items'

--- page-control.js
+++ page-control.js
@@ -1,6 +1,13 @@
 function PageControl() {
-
+    this.currentSelection = null;
 }
+
+let Data = Object.freeze({
+    target: 'data-control-target',
+    class: 'data-control-class',
+    group: 'data-control-group',
+    move: 'data-control-move'
+});
 
 PageControl.prototype.start = function start() {
     this.setupToggles();
@@ -10,27 +17,38 @@ PageControl.prototype.setupToggles = function setupToggles() {
     let classToggles = document.querySelectorAll('.toggle-class');
     Object.keys(classToggles).forEach(function (key) {
         let element = classToggles[key];
-        let toggle = element.getAttribute('data-control-target');
-        let className = element.getAttribute('data-control-class');
-        let group = element.getAttribute('data-control-group');
-        element.onclick = this.toggleClass(toggle, className, group);
+        let target = element.getAttribute(Data.target);
+        let className = element.getAttribute(Data.class);
+        let group = element.getAttribute(Data.group);
+        let toggle = target + '-' + group;
+        element.addEventListener('click', this.toggleClass(toggle, className, group));
     }.bind(this));
 };
 
 PageControl.prototype.toggleClass = function toggleClass(toggle, className, optional_group) {
     return function (event) {
         event.preventDefault();
+        document.querySelector('#' + toggle).classList.toggle(className);
         if (optional_group) {
-            let members = document.querySelectorAll('[data-control-group]');
+            let members = document.querySelectorAll('[' + Data.group + ']');
             Object.keys(members).forEach(function (key) {
                 let element = members[key];
-                let targetSelector = element.getAttribute('data-control-target');
-                let target = document.querySelector('#' + targetSelector);
-                if (toggle !== element.getAttribute(targetSelector)) {
+                let targetId = element.getAttribute(Data.target) + '-' + element.getAttribute(Data.group);
+                let target = document.querySelector('#' + targetId);
+                if (toggle !== targetId) {
                     target.classList.remove(className);
                 }
             });
         }
-        document.querySelector('#' + toggle).classList.toggle(className);
+        let navigations = document.querySelectorAll('.navigation-item');
+        Object.keys(navigations).forEach(function (key) {
+            let element = navigations[key];
+            element.classList.remove('highlighted');
+        });
+        let expanded = document.querySelector('.expanded');
+        if (expanded) {
+            let currentSelection = expanded.id.replace('-desc', '');
+            document.querySelector('.navigation-item.' + currentSelection).classList.add('highlighted');
+        }
     };
 };
